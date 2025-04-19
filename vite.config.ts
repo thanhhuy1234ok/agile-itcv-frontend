c594ded2edb9804a6658532c4656f829f00e07d0
thanhhuy1234ok <nguyenvothanhhuy2002@gmail.com>
2025-04-19 12:15:43 +0700

    fist init

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import react from "@vitejs/plugin-react"
+import * as path from "node:path"
+import { defineConfig } from "vitest/config"
+import packageJson from "./package.json" with { type: "json" }
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    open: true,
+    port: 3000,
+  },
+
+  test: {
+    root: import.meta.dirname,
+    name: packageJson.name,
+    environment: "jsdom",
+
+    typecheck: {
+      enabled: true,
+      tsconfig: path.join(import.meta.dirname, "tsconfig.json"),
+    },
+
+    globals: true,
+    watch: false,
+    setupFiles: ["./src/setupTests.ts"],
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler", // or "modern"
+        silenceDeprecations: [
+          "mixed-decls",
+          "color-functions",
+          "global-builtin",
+          "import",
+        ],
+      },
+    },
+  },
+})
