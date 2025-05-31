4061464a64a007a038da30b6219f6a5da2fd8750
thanhhuy1234ok <nguyenvothanhhuy2002@gmail.com>
2025-05-31 14:44:38 +0700

    fix: update refresh token URL to use production endpoint

--- src/services/axios.customer.js
+++ src/services/axios.customer.js
@@ -25,7 +25,7 @@ const createInstanceAxios = (baseURL) => {
 
                 try {
                     const res = await axios.get(
-                        "http://localhost:8080/api/v1/auth/refresh-token",
+                        "https://agile-itcv-backend.onrender.com/api/v1/auth/refresh-token",
                         { withCredentials: true }
                     );
 
